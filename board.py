5887a7275b3e26e2c02c765dbb7cbe5a35d7f96d
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-02 18:16:39 +0900

    Create board.py

---
+++ board.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext import commands
+import gspread_asyncio
+import gspread
+from discord.ui import Button, View
+from google.oauth2.service_account import Credentials
+import os
+import random
