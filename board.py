49946e560bf94627a1aeef9077dba28095479fe8
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-03 10:53:49 +0900

    Update board.py

--- board.py
+++ board.py
@@ -66,7 +66,7 @@ async def find_user(user, sheet7):
     return cell
 
 field_names = [
-    "Field 1", "Field 2", "Field 3", "Field 4", "Field 5",
+    "시작점", "Field 2", "Field 3", "Field 4", "Field 5",
     "Field 6", "Field 7", "Field 8", "Field 9", "Field 10",
     "Field 11", "Field 12", "Field 13", "Field 14", "Field 15",
     "Field 16", "Field 17", "Field 18", "Field 19", "Field 20",
