5809bdf1de70c7e8af243a0f3a40a445131006a3
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-09 14:19:29 +0900

    Update board.py

--- board.py
+++ board.py
@@ -4,6 +4,7 @@ import os
 import requests
 import random
 import gspread_asyncio
+import gspread.utils
 import re
 import time
 import pytz
@@ -182,7 +183,7 @@ class MissionView(discord.ui.View):
 
 
 async def get_column_values(sheet, col):
-    col_letter = gspread_asyncio.rowcol_to_a1(1, col)
+    col_letter = gspread.utils.rowcol_to_a1(1, col)
     data = await sheet.get(f'{col_letter}1:{col_letter}{sheet.row_count}')
     return [cell[0] for cell in data]
 
