539bffa5265415894136d19374fd0099181848ee
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-09 14:14:19 +0900

    Update board.py

--- board.py
+++ board.py
@@ -182,7 +182,9 @@ class MissionView(discord.ui.View):
 
 
 async def get_column_values(sheet, col):
-    return [cell.value for cell in await sheet.get_col(col, returnas="cell")]
+    col_letter = gspread.utils.rowcol_to_a1(1, col)
+    data = await sheet.get(f'{col_letter}1:{col_letter}{sheet.row_count}')
+    return [cell[0] for cell in data]
 
 
 async def get_random_missions(sheet):
