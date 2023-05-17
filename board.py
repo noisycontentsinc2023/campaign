23f409e8ea0d4b3d11c8763123841e7158d00eae
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-17 16:43:30 +0900

    Update board.py

--- board.py
+++ board.py
@@ -351,10 +351,10 @@ items = [
 @bot.command(name='상점')
 async def shop(ctx):
     embed = discord.Embed(title="포인트상점에 오신걸 환영합니다!", description=f"{ctx.author.mention}님 원하시는 품목을 선택해주세요!")
-    for i, item in enumerate(items):
-        embed.add_field(name=f"Item {i}", value=f"Cost: {item['cost']}", inline=False)
+    for item in items:
+        embed.add_field(name=item['name'], value=f"Cost: {item['cost']}", inline=False)
     await ctx.send(embed=embed)
-    await message.delete(delay=180)
+    await ctx.message.delete(delay=180)
 
 @bot.command(name='구매')
 async def buy(ctx, item_number: int):
